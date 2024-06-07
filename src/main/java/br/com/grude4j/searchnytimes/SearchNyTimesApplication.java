a5f0bcf33dd2e4cb29b16c91c2f4d61d125d3b74
Daniel Neves <danielnevesdecastro@gmail.com>
2024-06-06 23:44:51 -0300

    [FIRST COMMIT]

---
+++ src/main/java/br/com/grude4j/searchnytimes/SearchNyTimesApplication.java
@@ -0,0 +1,13 @@
+package br.com.grude4j.searchnytimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchNyTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchNyTimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grude4j/searchnytimes/SearchNyTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grude4j.searchnytimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchNyTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
